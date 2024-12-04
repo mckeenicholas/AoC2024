937ad9e57b65e8599e2a9722bd48cda49bee950c
Nicholas McKee <nmsmckee@gmail.com>
2024-12-04 12:56:58 -0500

    Day 3

--- 3/p1.rkt
+++ 3/p1.rkt
@@ -1,8 +1,17 @@
 #lang racket
 
-(define input-lines
-  (with-input-from-file "input.txt"
-    (lambda ()
-      (port->lines (current-input-port)))))
+(define get-input (with-input-from-file "input.txt" (lambda () (port->lines (current-input-port)))))
 
-(for-each displayln input-lines)
+(define (extract-instructions input)
+  (map (lambda (expr)
+         (match expr
+           [(list x y)
+            (let ([num-x (string->number x)]
+                  [num-y (string->number y)])
+              (* num-x num-y))]))
+       (regexp-match* #rx"mul\\(([0-9]+),([0-9]+)\\)" input #:match-select rest)))
+
+(define (process-instructions lines)
+  (foldl + 0 (map (lambda (line) (foldl + 0 (extract-instructions line))) lines)))
+
+(displayln (process-instructions get-input))
